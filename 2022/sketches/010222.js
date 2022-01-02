ed2e2b57c6d6bf6a4ab41bb2ecb63d4219447101
lwlsn <ewilson01@protonmail.ch>
2022-01-02 20:11:56 +0000

    02/01/22

---
+++ 2022/sketches/010222.js
@@ -0,0 +1,39 @@
+// Sunday 2nd January 
+// Hangover day 2 
+
+
+let colorArray = ["#0a0a0a","#ffffff", "#ffffff", "#ffffff", "#ff0505"];
+
+function setup() {
+    createCanvas(600, 600);
+
+    frameRate(5);
+
+}
+
+
+function draw() {
+
+    background(0);
+
+    push();
+
+    for (let i=0; i< width; i+= 20) {
+        for (let j=0; j < height; j+= 20){
+            
+            let scaleX = map(mouseX, 0, width, 0.15, 1);
+            let scaleY = map(mouseY, 0, height, 0.15, 1);
+        
+            rotate(PI/2);
+            fill(random(colorArray));
+            rect(i, j, 20*scaleX, 20*scaleY);
+          
+      
+        }
+    }
+
+    pop();
+
+
+
+}
