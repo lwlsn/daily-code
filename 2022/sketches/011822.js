bf9fb58f3f49169bce06f4b304eb7e16017551b7
lwlsn <ewilson01@protonmail.ch>
2022-01-19 15:05:51 +0000

    superposition 19/12/22

---
+++ 2022/sketches/011822.js
@@ -0,0 +1,50 @@
+// Tuesday 18th January 
+
+// Got so much to do but here I am
+
+let x1 = 10, y1= 10;
+let size= 20;
+
+function setup() {
+    createCanvas(600, 600);
+    noFill();
+    stroke(255);
+    background(0);
+}
+
+
+function draw() {
+
+    let level = size;
+
+    let r = random(1);
+    let n = noise(random(1));
+    print(n);
+
+    if (r < 0.5) {
+        stroke(255);
+        noFill();
+        ellipse(x1, y1, size, size);
+
+    } else {
+        noStroke();
+        fill(255);
+        ellipse(x1, y1, size, size);
+    }
+
+
+    x1 += level; 
+
+    if (x1 >= width){
+        x1 = 10;
+        y1 += level;
+    }
+
+    if (y1 >= height) {
+     
+        x1 = 10; y1= 10;
+        background(0);
+
+    }
+
+}
---
+++ 2022/sketches/011922.js
@@ -0,0 +1,95 @@
+// Wednesday 19th January
+// Todays inspo https://www.instagram.com/p/CQGH5QKFslG/
+
+
+let lineCoords = [];
+let numLines = 10;
+let circleMax = 400;
+
+let x,y;
+
+
+function setup(){
+    createCanvas(600, 600);
+
+
+    for (let i = 0; i < numLines; i ++ ) {
+        lineCoords[i] = createVector(random(circleMax/2, circleMax/2+100), random(TWO_PI) );    
+    }
+  
+
+}
+
+function draw() {
+    background(0, 180);
+
+    // background grid
+    stroke(255, 5);
+    strokeWeight(5);
+ 
+ 
+    for (let i=0; i < height; i+= 20 ) {
+        for (let j=0; j < width; j += 50) {
+       
+            line(0,i, width,i);
+            line(j,0, j, height);
+        }
+       
+    }  
+    
+    
+    // Axes 
+    stroke(255, 200);
+    noFill();
+    strokeWeight(1);
+    line(0, height/2, width, height/2);
+    line(width/2, 0, width/2, height);
+    ellipse(width/2, height/2, circleMax, circleMax);
+   // Spanning Lines 
+
+  
+ 
+    translate(width/2, height/2);
+    for (let i=0; i < numLines; i++) {
+   
+        x=lineCoords[i].x * cos(lineCoords[i].y);
+        y=lineCoords[i].x * sin(lineCoords[i].y);
+        push(); 
+            translate(x, y);
+            rectMode(CENTER);
+            noStroke();
+            fill(0);
+            ellipse(0,0, 50, 50);
+            stroke(255, 200);
+            noFill();
+            strokeWeight(1);
+            line(-10, 0, 10, 0);
+            line(0, -10, 0, 10);
+            rect(0, 0, 5, 5);
+            
+        pop();
+
+        line(0, 0, x , y );
+
+        // More concise way of expressing this? 
+        if (lineCoords[i].y > 0 && lineCoords[i].y <= PI/2 ) {
+            arc(0, 0, lineCoords[i].x*0.4, lineCoords[i].x*0.4, lineCoords[i].y , PI/2); 
+        }
+
+        else if (lineCoords[i].y > PI/2 && lineCoords[i].y <= PI ) {
+            arc(0, 0, lineCoords[i].x*0.4, lineCoords[i].x*0.4, lineCoords[i].y , PI);
+        }
+
+        else if (lineCoords[i].y > PI && lineCoords[i].y <= 3*PI/2 ) {
+            arc(0, 0, lineCoords[i].x*0.4, lineCoords[i].x*0.4, lineCoords[i].y , 3*PI/2);
+        }
+       
+
+       else {
+        arc(0, 0, lineCoords[i].x*0.4, lineCoords[i].x*0.4, lineCoords[i].y , 2*PI);
+       } 
+    }
+
+    
+
+}
