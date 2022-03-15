6662dd72a0a68fb16a2999f79433264e3f4d3d0d
lwlsn <ewilson01@protonmail.ch>
2022-03-15 17:31:22 +0000

    15/03 updates

--- 2022/sketches/031522.js
+++ 2022/sketches/031522.js
@@ -1,19 +1,21 @@
 // Tuesday 15th March 
-let DIM = 12;
+
+// Accidental hallucogenic 
+let DIM = 8;
 
 let t = 0.01; 
 
 function setup() {
     createCanvas(600, 600, WEBGL);
     noFill();
-    stroke(255,0,0);
+    stroke(255,0,0); 
 }
 
 function draw() {
     background(0);
 
     
-    rotateY(t);
+    // rotateY(t);
     rotateZ(t);
 
     for (let i=0; i < DIM; i++) {
@@ -21,10 +23,10 @@ function draw() {
             for (let k=0; k < DIM; k++) {
                 let x = map(i, 0, DIM, -width/2, width/2);
                 let y = map(j, 0, DIM, -height/2, height/2);
-                let z = map(k, 0, DIM, -100, 100);
+                let z = map(k, 0, DIM, -height/2, height/2);
 
-                point(x, y,z);
-                circle(x, y,z);
+                // point(x, y, z);
+                circle(x, y,z, 100);
             }
         
         }
