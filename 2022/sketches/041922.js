4cdb68e8a87c4241a8366b284b9cd7f2a8b4baf9
lwlsn <ewilson01@protonmail.ch>
2022-04-19 12:22:41 +0100

    19/04/22, multiple onscreen blobs

--- 2022/sketches/041922.js
+++ 2022/sketches/041922.js
@@ -1,52 +1,55 @@
-// Thursday 14th April
+// Tuesday 19th April
 
-// Inspoo.. ?
 
 let kMax;
-let step;
+let step = [];
 let n = 80; // number of blobs
 let radius = 0; // diameter of the circle
 let inter = 0.1; // difference between the sizes of two blobs
-let maxNoise = 500;
+let maxNoise = [];
 
 let noiseProg = (x) => (x);
-
 
 function setup() {
     createCanvas(600, 600);
     angleMode(RADIANS);
     kMax = random(0.6, 1.0);
-    step = 0.01;
+    // step = 0.01;
     noStroke();
+
+    for (let i=0; i<6; i++) {
+        maxNoise[i] = random(150, 250);
+        step[i] = random(0.1);
+    }
 }
 
 function draw() {
-    blendMode(DIFFERENCE);
-    background(0);
-    blendMode(BLEND);
 
+    background(0);
+    // blendMode(SOFT_LIGHT);
 
     let t = frameCount/100;
-
 
     for (let i = n; i > 0; i--) {
 		let alpha = pow(1 - noiseProg(i / n), 3);
 		let size = radius + i * inter;
 		let k = kMax * sqrt(i/n);
-		let noisiness = maxNoise * noiseProg(i / n);
 
-        fill(103, 89, 122, alpha*255);
-        blob(size, width/2, height/2, k, t - i * step, noisiness);
-
-        fill(110, 136, 148, alpha*255); 
-        blob(size, width/2, height/2,k, t - i * step+1, noisiness );
-
-        fill(133, 186, 161, alpha*255); 
-        blob(size, width/2, height/2, k, t - i * step+2, noisiness );
+    
+        for (let y = 50; y < height; y += 150) {
+            for (let x = 150; x < width; x += 150) {
+                let noisiness = maxNoise[(y-50)/150] * noiseProg(i / n);
+                fill((100+x)/5, (122+y)/5, 163, alpha*255);
+                blob(size, x, y, k, t - i * step[(y-50)/150], noisiness);
+            }
+        } 
+  
     }
 
 
+    if (frameCount%100 == 0) setup();
 }
+
 
 function blob(size, xCenter, yCenter, k, t, noiseV) {
     beginShape();
