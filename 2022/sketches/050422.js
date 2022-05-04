1de7a208ae6b5d0f490e19023e13e18266bad0b0
lwlsn <ewilson01@protonmail.ch>
2022-05-04 16:00:33 +0100

    04/05/22 networks

---
+++ 2022/sketches/050422.js
@@ -0,0 +1,43 @@
+// Wednesday 4 May
+// Networks 1 
+
+let count = 30, margin = 50, sw=1, rad=10;
+
+let x = [], y = [], speedX = [], speedY = [];
+
+
+function setup() {
+    createCanvas(600, 600);
+    // noLoop();
+    rectMode(CENTER);
+
+      // Get node co-ordinates
+      for (let i=0; i< count; i++) {
+        x[i] = random(margin, width - margin);
+        y[i] = random(margin, height-margin);
+  
+    }
+}
+
+function draw() {
+    background(0);
+
+    stroke(255);
+
+  
+
+
+    for (let i=0; i<count-1; i++) {
+        for (let j=0; j< count-1; j++) {
+            strokeWeight(sw);
+            line(x[i],y[i],x[j],y[j]);
+            fill(0);
+            rect(x[i],y[i],rad,rad);
+
+            speedX[i] = random(-1, 1);
+            speedY[i] = random(-1, 1);
+            x[i] += speedX[i];
+            y[i] += speedY[i];
+        }
+    }
+}
